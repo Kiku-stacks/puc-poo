5664471e81398912aad4e9eb8d58ee9b7d483504
Kiku-stacks <brunobentosilva459@gmail.com>
2026-08-21 18:18:48 -0300

    Create Mensagem.java with header printing method
    Implement a Java class that prints a formatted header.

---
+++ exercicios/aula dia 18/Mensagem.java
@@ -0,0 +1,18 @@
+package com.bruno.pucgo.exercicios.aula_dia_18;
+
+public class Mensagem {
+
+    public static void main(String[] args) {
+        cabecalho();
+    }
+
+    public static void cabecalho(){
+        System.out.println("+--------------------+");
+        System.out.println("|  xxxx  xxx   xxxx  |");
+        System.out.println("|  x  x  x  x  x     |");
+        System.out.println("|  xxxx  x  x  xxxx  |");
+        System.out.println("|  x  x  x  x     x  |");
+        System.out.println("|  x  x  xxx   xxxx  |");
+        System.out.println("+--------------------+");
+    }
+}
