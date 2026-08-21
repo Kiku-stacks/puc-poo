c875b3c5c732910d272f77f688e1f3dfaa020055
Kiku-stacks <brunobentosilva459@gmail.com>
2026-08-21 18:19:07 -0300

    Create SegundosDia.java

---
+++ exercicios/aula dia 18/SegundosDia.java
@@ -0,0 +1,13 @@
+package com.bruno.pucgo.exercicios.aula_dia_18;
+
+public class SegundosDia {
+
+    public static void main(String[] args) {
+
+        System.out.println(calcularSegundosDia());
+    }
+
+    public static int calcularSegundosDia(){
+        return 24*60*60;
+    }
+}
